b6651558c30d6dd3fc2e546f8efcac5e118ebc75
Alex Stamat <stamalex2000@gmail.com>
2025-07-20 19:45:30 +0300

    styling

--- src/UI/MyButton.tsx
+++ src/UI/MyButton.tsx
@@ -16,7 +16,7 @@ export const MyButton: React.FC<MyButtonProps> = ({
     <Button
       {...props}
       className={clsx(
-        'inline-flex items-center gap-2 rounded-md bg-gray-300 dark:bg-gray-800 active:bg-gray-400 dark:active:bg-gray-900 px-3 py-1.5 text-sm/6 font-semibold text-gray-800 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600 cursor-pointer transition-colors duration-200',
+        'select-none inline-flex items-center gap-2 rounded-md bg-gray-300 dark:bg-gray-800 active:bg-gray-400 dark:active:bg-gray-900 px-3 py-1.5 text-sm/6 font-semibold text-gray-800 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600 cursor-pointer transition-colors duration-200',
         className
       )}
     >
